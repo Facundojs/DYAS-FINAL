15d0b6be68d84c4d81c2d113b110e55b64962a9f
Ariel <ariel.ras.90@gmail.com>
2023-12-01 07:18:28 -0300

    Se agrega en DAO metodos: ActualizarOrganizador, AltaOrganizador

--- DAO/OrganizadorDao.cs
+++ DAO/OrganizadorDao.cs
@@ -10,6 +10,66 @@ namespace DAO
 {
     public class OrganizadorDao
     {
+
+        public void AltaOrganizador(OrganizadorEntity nuevoOrganizador, string password)
+        {
+            try
+            {
+                using (ContextDb context = new ContextDb())
+                {
+                    var organizadorDb = new ORGANIZADOR
+                    {
+                        NOMBRE = nuevoOrganizador.Nombre,
+                        APELLIDO = nuevoOrganizador.Apellido,
+                        DNI = nuevoOrganizador.Dni,
+                        EMAIL = nuevoOrganizador.Email,
+                        PASSWORD = password
+                    };
+
+                    context.ORGANIZADOR.Add(organizadorDb);
+
+                    context.SaveChanges();
+                }
+            }
+            catch { throw; }
+        }
+
+        public void ActualizarOrganizador (OrganizadorEntity organizador)
+        {
+            try
+            {
+                using (ContextDb context = new ContextDb())
+                {
+                    var organizadorDb = context.ORGANIZADOR.SingleOrDefault(O => O.ID_ORGANIZADOR == organizador.CodigoOrganizador) 
+                        ?? throw new Exception("No se encuentra el organizador seleccionado");
+
+                    organizadorDb.NOMBRE = organizador.Nombre;
+                    organizadorDb.APELLIDO = organizador.Apellido;
+                    organizadorDb.DNI = organizador.Dni;
+                    organizadorDb.EMAIL = organizador.Email;
+
+                    context.SaveChanges();
+                }
+            }
+            catch { throw; }
+        }
+
+        public void ActualizarPassword (string email, string password)
+        {
+            try
+            {
+                using (ContextDb context = new ContextDb())
+                {
+                    var organizadorDb = context.ORGANIZADOR.SingleOrDefault(O => O.EMAIL == email) ?? throw new Exception("No se encuentra el organizador seleccionado");
+
+                    organizadorDb.PASSWORD = password;
+
+                    context.SaveChanges();
+                }
+            }
+            catch { throw; }
+        }
+
         public OrganizadorEntity ObtenerOrganizador(string usuario)
         {
             try
